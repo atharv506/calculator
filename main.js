fb89afd75ac7ee97de214c2e1a67f96aa6d96a05
atharv goyal <atharv.goyoal506@gmail.com>
2025-06-06 02:34:46 +0530

    js logic

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-
-let number = document.querySelectorAll('.');
+let string = "";
+let buttons = document.querySelectorAll('#button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (f) =>{
+        if (f.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (f.target.innerHTML == 'AC') {
+            string = "";   
+            document.querySelector('input').value = string;
+        }
+        else if (f.target.innerHTML == 'DEL') {
+            string = string.slice(0, -1); 
+            document.querySelector('input').value = string;
+        }
+        else {
+            
+console.log(f.target)
+        string = string + f.target.innerHTML;
+        document.querySelector('input').value = string;
+    }
+})
+});
